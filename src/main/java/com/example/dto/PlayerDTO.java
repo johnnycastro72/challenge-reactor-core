1f1213a650cb35f0ffc104754b9029dfde5d0d87
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-27 15:46:43 -0500

    DTO
    Se crea una clase DTO para player

---
+++ src/main/java/com/example/dto/PlayerDTO.java
@@ -0,0 +1,12 @@
+package com.example.dto;
+
+public class PlayerDTO {
+    public int id;
+    public String name;
+    public int age;
+    public String icon;
+    public String national;
+    public int winners;
+    public int games;
+    public String club;
+}

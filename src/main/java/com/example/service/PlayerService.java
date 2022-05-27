389b45491941acfde7946ea3a08120e30577846a
Adryan <adryanynfante@gmail.com>
2022-05-27 15:05:00 -0500

    filtro de jugadores  >= 35
    filtro de Jugadores Mayores A 35

--- src/main/java/com/example/service/PlayerService.java
+++ src/main/java/com/example/service/PlayerService.java
@@ -1,4 +1,11 @@
 package com.example.service;
 
+import com.example.demo.Player;
+import reactor.core.publisher.Flux;
+
 public class PlayerService {
+
+    public Flux<Player> ObtenerJugadoresMayoresA35(Flux<Player> lista){
+        return lista.filter(player -> player.age >= 35 );
+    }
 }

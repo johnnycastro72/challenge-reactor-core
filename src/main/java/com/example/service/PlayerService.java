fbe956aafb6808df088567d24ef95ec494db73a0
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-27 16:21:26 -0500

    Database service
    Save, FindAll and findbyid services created

--- src/main/java/com/example/service/PlayerService.java
+++ src/main/java/com/example/service/PlayerService.java
@@ -7,6 +7,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class PlayerService {
@@ -20,6 +21,19 @@ public class PlayerService {
     public Flux<Player> ObtenerJugadoresMayoresA35(Flux<Player> playerList){
         return playerList.filter(player -> player.age >= 35 );
     }
+
+    public Mono<PlayerDTO> savePlayer (PlayerDTO p){
+        return this.iPlayerRepository.save(convertDTOToEntity(p)).map(playerDto -> convertEntityToDTO(playerDto));
+    }
+
+    public Flux<PlayerDTO> findAllPlayers() {
+        return this.iPlayerRepository.findAll().map( p -> convertEntityToDTO(p));
+    }
+
+    public Mono<PlayerDTO> findPlayerById(Integer id){
+        return this.iPlayerRepository.findById(id).map(p -> convertEntityToDTO(p) );
+    }
+
     //Converters
     public PlayerDTO convertEntityToDTO(Player p){
         return modelMapper.map(p, PlayerDTO.class);

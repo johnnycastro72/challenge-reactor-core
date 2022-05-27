127f28e25709d7a8a1f211a79cc9192003eed174
Adryan <adryanynfante@gmail.com>
2022-05-27 16:07:13 -0500

    Update PlayerService.java

--- src/main/java/com/example/service/PlayerService.java
+++ src/main/java/com/example/service/PlayerService.java
@@ -8,5 +8,5 @@ public class PlayerService {
     public Flux<Player> ObtenerJugadoresMayoresA35(Flux<Player> playerList){
         return playerList.filter(player -> player.age >= 35 );
     }
-    
+
 }

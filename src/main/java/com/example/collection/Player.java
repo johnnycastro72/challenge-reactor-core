51e768c6d0369373b27f5d63baf88d6d6c1ff640
Adryan <adryanynfante@gmail.com>
2022-05-27 21:27:45 -0500

    pruebas unitarias
    Pruebas OK, verificadas

--- src/main/java/com/example/collection/Player.java
+++ src/main/java/com/example/collection/Player.java
@@ -50,4 +50,68 @@ public class Player {
                 ", club='" + club + '\'' +
                 '}';
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getNational() {
+        return national;
+    }
+
+    public void setNational(String national) {
+        this.national = national;
+    }
+
+    public int getWinners() {
+        return winners;
+    }
+
+    public void setWinners(int winners) {
+        this.winners = winners;
+    }
+
+    public int getGames() {
+        return games;
+    }
+
+    public void setGames(int games) {
+        this.games = games;
+    }
+
+    public String getClub() {
+        return club;
+    }
+
+    public void setClub(String club) {
+        this.club = club;
+    }
 }
